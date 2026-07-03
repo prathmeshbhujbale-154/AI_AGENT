e472409e3221f378f31a3838b51af156f85df1e6
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-07-03 19:15:54 +0530

    build agent

--- agent.py
+++ agent.py
@@ -1,2 +1,11 @@
 from agno.agent import Agent
 from agno.models.groq import Groq
+
+def build_agent() :
+    return Agent (
+        model = Groq(id = "")
+        markdown = True # gives output in proper format
+        instructions ="you are helpful and expert travel agent."
+    )
+agent = build_agent()
+
