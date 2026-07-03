733ce9e50585db47bfff7521757cccb1fa44a289
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-07-03 22:00:41 +0530

    imported ddgs and tools and model is changed

--- agent.py
+++ agent.py
@@ -1,12 +1,14 @@
 from agno.agent import Agent
 from agno.models.groq import Groq
 from dotenv import load_dotenv
+from agno.tools.duckduckgo import DuckDuckGoTools
 
 load_dotenv("../.env")
 
 def build_agent() :
     return Agent (
-        model = Groq(id = "llama-3.3-70b-versatile"),
+        model = Groq(id = "qwen/qwen3-32b"),
+        tools=[DuckDuckGoTools()],
         markdown = True, # gives output in proper format
         instructions ="you are helpful and expert travel agent.",
         add_datetime_to_context=True
