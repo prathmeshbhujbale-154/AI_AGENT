d3cde9dd8b074abf81e2e6f197781f50764b61cf
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-07-03 21:48:23 +0530

    execute current date and time

--- agent.py
+++ agent.py
@@ -9,7 +9,8 @@ def build_agent() :
         model = Groq(id = "llama-3.3-70b-versatile"),
         markdown = True, # gives output in proper format
         instructions ="you are helpful and expert travel agent.",
+        add_datetime_to_context=True
     )
 agent = build_agent()
 
-agent.print_response("my budget is 1L INR , shouldi travel to goa or phuket")
+agent.print_response("my budget is 1L INR , should i travel to goa or phuket")
