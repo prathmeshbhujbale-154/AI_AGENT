2bf89bc1a0a3adbd02a85dc1e7779f50e53f3aa7
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-07-04 19:09:37 +0530

    team created , asked new question

--- teams.py
+++ teams.py
@@ -9,16 +9,12 @@ eng_agent = Agent(name="English Agent" , role="you answer questions in english")
 frc_agent = Agent(name="french Agent" , role="you answer questions in french")
 hin_agent = Agent(name="hindi Agent" , role="you answer questions in hindi")
 
+team_leader = Team(
+    name = "answer and translation team",
+    members = [eng_agent, frc_agent, hin_agent],
+    model = Groq(id = "qwen/qwen3-32b"),
+    markdown=True,
+    show_members_responses=True
+)
 
-
-def build_agent() :
-    return Agent (
-        model = Groq(id = "qwen/qwen3-32b"),
-        tools=[],
-        markdown = True, # gives output in proper format
-        instructions ="you are helpful and expert travel agent.",
-        add_datetime_to_context=True
-    )
-agent = build_agent()
-
-agent.print_response("my budget is 1L INR , should i travel to goa or phuket")
+team_leader.print_response("who is prime minister  of india")
