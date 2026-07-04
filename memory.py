fdc9c753089bc0709ece939bd24eabc7dd111dca
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-07-04 19:47:27 +0530

    created new agent file that saves previous memory

---
+++ memory.py
@@ -0,0 +1,17 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
+
+def build_agent() :
+    return Agent (
+        model = Groq(id = "qwen/qwen3-32b"),
+        tools=[DuckDuckGoTools()],
+        markdown = True, # gives output in proper format
+        instructions ="you are helpful and expert travel agent.",
+        add_datetime_to_context=True
+    )
+agent = build_agent()
+
+agent.print_response("my budget is 1L INR , should i travel to goa or phuket")
